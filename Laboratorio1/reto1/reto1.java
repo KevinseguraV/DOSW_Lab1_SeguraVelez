631affc9dffb25a9837502ce42070db0233ebcf7
Juan Pablo Velez Muñoz <juan.velez-mu@mail.escuelaing.edu.co>
2026-02-03 21:32:42 -0500

    correccion del reto1

--- Laboratorio1/reto1/reto1.java
+++ Laboratorio1/reto1/reto1.java
@@ -1,8 +1,10 @@
+package Laboratorio1.reto1;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-public class Reto1 {
+public class reto1 {
 
     public static void main(String[] args) {
 
