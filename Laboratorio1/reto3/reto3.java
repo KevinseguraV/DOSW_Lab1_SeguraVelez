c65e9e8bd36d5386edc60453deb6a56066abc957
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-05 18:10:08 -0500

    Implementación reto 3

--- Laboratorio1/reto3/reto3.java
+++ Laboratorio1/reto3/reto3.java
@@ -1,1 +1,24 @@
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Reto3 {
+
+    public static String ecoMisterioso(String mensaje) {
+
+        Function<String, String> repetir = msg ->
+                Stream.generate(() -> msg)
+                        .limit(3)
+                        .collect(Collectors.joining(" "));
+
+        String resultado = repetir.apply(mensaje);
+
+        StringBuilder sb = new StringBuilder(resultado);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ecoMisterioso("Hola"));
+    }
+}
 
