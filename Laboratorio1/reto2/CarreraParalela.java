487e547cf04084cada1381d4b12a33be4d16d571
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-04 20:18:36 -0500

    Carril 1: implementacion con lambda para obtener el numero mayor

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -3,5 +3,10 @@ package Laboratorio1.reto2;
 import java.util.List;
 
 public class CarreraParalela {
-    
+
+    public static int obtenerMayor(List<Integer> numeros) {
+        return numeros.stream()
+                .max(Integer::compareTo)
+                .orElse(0);
+    }
 }
