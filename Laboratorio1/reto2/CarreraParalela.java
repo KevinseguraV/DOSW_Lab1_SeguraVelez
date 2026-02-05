604f0052eb3f738df085de6ae3193ef89850cb2f
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-04 22:35:31 -0500

    carril 1: choque 2 resuelto completo

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -21,6 +21,8 @@ public class CarreraParalela {
         r.setMenor(obtenerMenor(numeros));
         r.setCantidad(cantidadDatos(numeros));
         r.setEsDivisorDe2(2 % r.getMayor() == 0 ? true : false);
+        r.setMultiploDe2 ( r.getMayor() % 2 == 0 ? true : false);
+
         return r;
     }
 
