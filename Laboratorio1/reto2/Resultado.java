4afb0473404b23d4d2b865bfe3aff97f05fe8c43
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-04 20:56:06 -0500

    adicion funcion procesar lista

--- Laboratorio1/reto2/Resultado.java
+++ Laboratorio1/reto2/Resultado.java
@@ -1,5 +1,6 @@
 package Laboratorio1.reto2;
 
+<<<<<<< HEAD
 public class Resultado {
     private int mayor;
     private int menor;
@@ -18,4 +19,29 @@ public class Resultado {
     public void setMenor(int menor) {
         this.menor = menor;
     }
+=======
+import java.util.List;
+
+public class Resultado {
+private int mayor;
+private int menor;
+private int cantidad;
+
+public Resultado() {}
+
+
+public void setCantidad(int cantidad) {
+    this.cantidad = cantidad;
+}
+
+public void setMayor(int mayor) {
+    this.mayor = mayor;
+}
+
+public void setMenor(int menor) {
+    this.menor = menor;
+}
+
+
+>>>>>>> 14207c5 (adicion funcion procesar lista)
 }
