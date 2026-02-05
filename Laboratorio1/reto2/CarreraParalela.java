6ac6c15c897e7f46f1df2ce13531e09463603b8e
Juan Pablo Velez Muñoz <juan.velez-mu@mail.escuelaing.edu.co>
2026-02-04 23:35:53 -0500

    llegada a la meta

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -27,6 +27,15 @@ public class CarreraParalela {
         return r;
     }
 
+    public static void procesarDosListas(List<Integer> lista1, List<Integer> lista2) {
+        Resultado r1 = procesarLista(lista1);
+        Resultado r2 = procesarLista(lista2);
+        System.out.print( "Resultado lista 1: \n");
+        System.out.println(r1.toString());
+        System.out.print( "Resultado lista 2: \n");
+        System.out.println(r2.toString());
+    }
+
     public static int cantidadDatos(List<Integer> numeros) {
         return numeros.size();
     }
