fa91bac7a3d8b339391572d8ea9c2ceba231f944
Juan Pablo Velez Muñoz <juan.velez-mu@mail.escuelaing.edu.co>
2026-02-05 20:06:33 -0500

    método de cargar el mapa con Hashtable

---
+++ Laboratorio1/reto4/Reto4.java
@@ -0,0 +1,15 @@
+package Laboratorio1.reto4;
+
+public class Reto4 {
+    public static void main(String[] args) {
+
+    }
+
+    public static Map<String, Integer> cargarMapa(List<Map.Entry<String, Integer>> datos) {
+        Map<String, Integer> tabla = new Hashtable<>();
+        for (Map.Entry<String, Integer> entry : datos) {
+            tabla.putIfAbsent(entry.getKey(), entry.getValue());
+        }
+        return tabla;
+    }
+}
