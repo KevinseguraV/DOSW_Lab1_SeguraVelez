fc4f704d9263f5af96907584f61e55702fdd8c74
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-05 20:49:53 -0500

    funcion de imprimir mayusculas

--- Laboratorio1/reto4/Reto4.java
+++ Laboratorio1/reto4/Reto4.java
@@ -2,12 +2,9 @@ package Laboratorio1.reto4;
 
 import java.util.*;
 
-public class Reto4 {
-    public static void main(String[] args) {
+public class reto4 {
 
-    }
-
-    public static Map<String, Integer> cargarMapaHash(List<Map.Entry<String, Integer>> datos) {
+    public static Map<String, Integer> cargarMapa(List<Map.Entry<String, Integer>> datos) {
         Map<String, Integer> mapa = new HashMap<>();
         for (Map.Entry<String, Integer> entry : datos) {
             mapa.putIfAbsent(entry.getKey(), entry.getValue());
@@ -15,21 +12,19 @@ public class Reto4 {
         return mapa;
     }
 
-    public static Map<String, Integer> cargarMapaTable(List<Map.Entry<String, Integer>> datos) {
-        Map<String, Integer> tabla = new Hashtable<>();
-        for (Map.Entry<String, Integer> entry : datos) {
-            tabla.putIfAbsent(entry.getKey(), entry.getValue());
-        }
-        return tabla;
-    }
-
     public static Map<String, Integer> combinarMapas(
         Map<String, Integer> hashMap,
         Map<String, Integer> hashTable) {
 
-        Map<String, Integer> resultado = new HashMap<>(hashMap);
-        resultado.putAll(hashTable); // prioriza Hashtable
-        return resultado;
+    Map<String, Integer> resultado = new HashMap<>(hashMap);
+    resultado.putAll(hashTable); // prioriza Hashtable
+    return resultado;
+}
+
+ public static void imprimirMayusculas(Map<String, Integer> mapa) {
+        mapa.forEach((k, v) ->
+                System.out.println("Clave: " + k.toUpperCase() + " | Valor: " + v)
+        );
     }
 
 }
