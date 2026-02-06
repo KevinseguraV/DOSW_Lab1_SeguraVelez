1bc1b8c95497aaed6b502c068834c02b529a5b8c
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-05 21:47:37 -0500

    Implementación HashSet reto 5

--- Laboratorio1/reto5/reto5.java
+++ Laboratorio1/reto5/reto5.java
@@ -1,1 +1,27 @@
+package Laboratorio1.reto5;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class reto5 {
+
+    public static Set<Integer> cargarGuerreros() {
+
+        Set<Integer> guerreros = new HashSet<>(
+                Arrays.asList(4, 9, 15, 7, 18, 21, 10, 5)
+        );
+
+        return guerreros.stream()
+                .filter(n -> n % 3 != 0)
+                .collect(Collectors.toSet());
+    }
+
+  
+    public static Set<Integer> unirConjuntos(Set<Integer> a, Set<Integer> b) {
+        Set<Integer> resultado = new TreeSet<>();
+        resultado.addAll(a);
+        resultado.addAll(b);
+        return resultado;
+    }
+}
 
