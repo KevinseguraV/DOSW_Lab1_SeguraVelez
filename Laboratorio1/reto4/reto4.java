5099d51ab2a19a8e5690a918c50244241419bcae
Kevin Segura <kevinjairseguravelandia@gmail.com>
2026-02-05 20:07:56 -0500

    implementacion con HashTable

--- Laboratorio1/reto4/reto4.java
+++ Laboratorio1/reto4/reto4.java
@@ -1,1 +1,15 @@
+package Laboratorio1.reto4;
+
+import java.util.*;
+
+public class reto4 {
+
+    public static Map<String, Integer> cargarMapa(List<Map.Entry<String, Integer>> datos) {
+        Map<String, Integer> mapa = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : datos) {
+            mapa.putIfAbsent(entry.getKey(), entry.getValue());
+        }
+        return mapa;
+    }
+}
 
